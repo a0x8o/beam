c0df6aa48726a01fc4a99e2c5977fcd9a0ca7a09
Alex Barreto <axbarreto@lavabit.com>
2019-04-13 16:13:19 -0700

    [BEAM-6942]  Make modifications to pipeline options to be visible to all views. (#8225)
    * Fixes a bug in pipeline option that prevents changes of multi-valued options to be propagated across views. Also, adds documentation and unit tests for codepath affected by the bug and the fix.
    
    * Fix typos in flags defined in PortableOptions.
    
    * Check that views do not expose attributes defined by other views.

--- runners/spark/src/main/java/org/apache/beam/runners/spark/translation/SparkBatchPortablePipelineTranslator.java
+++ runners/spark/src/main/java/org/apache/beam/runners/spark/translation/SparkBatchPortablePipelineTranslator.java
@@ -22,10 +22,12 @@ import static org.apache.beam.runners.fnexecution.translation.PipelineTranslator
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import javax.annotation.Nullable;
 import org.apache.beam.model.pipeline.v1.RunnerApi;
+import org.apache.beam.model.pipeline.v1.RunnerApi.ExecutableStagePayload.SideInputId;
 import org.apache.beam.model.pipeline.v1.RunnerApi.PCollection;
 import org.apache.beam.runners.core.SystemReduceFn;
 import org.apache.beam.runners.core.construction.PTransformTranslation;
@@ -54,6 +56,8 @@ import org.apache.beam.vendor.guava.v20_0.com.google.common.collect.Iterables;
 import org.apache.spark.HashPartitioner;
 import org.apache.spark.Partitioner;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.broadcast.Broadcast;
+import scala.Tuple2;
 
 /** Translates a bounded portable pipeline into a Spark job. */
 public class SparkBatchPortablePipelineTranslator {
@@ -81,6 +85,9 @@ public class SparkBatchPortablePipelineTranslator {
         SparkBatchPortablePipelineTranslator::translateGroupByKey);
     translatorMap.put(
         ExecutableStage.URN, SparkBatchPortablePipelineTranslator::translateExecutableStage);
+    translatorMap.put(
+        PTransformTranslation.FLATTEN_TRANSFORM_URN,
+        SparkBatchPortablePipelineTranslator::translateFlatten);
     this.urnToTransformTranslator = translatorMap.build();
   }
 
@@ -163,7 +170,7 @@ public class SparkBatchPortablePipelineTranslator {
     context.pushDataset(getOutputId(transformNode), new BoundedDataset<>(groupedByKeyAndWindow));
   }
 
-  private static <InputT, OutputT> void translateExecutableStage(
+  private static <InputT, OutputT, SideInputT> void translateExecutableStage(
       PTransformNode transformNode, RunnerApi.Pipeline pipeline, SparkTranslationContext context) {
 
     RunnerApi.ExecutableStagePayload stagePayload;
@@ -180,8 +187,22 @@ public class SparkBatchPortablePipelineTranslator {
     Map<String, String> outputs = transformNode.getTransform().getOutputsMap();
     BiMap<String, Integer> outputMap = createOutputMap(outputs.values());
 
-    SparkExecutableStageFunction<InputT> function =
-        new SparkExecutableStageFunction<>(stagePayload, context.jobInfo, outputMap);
+    ImmutableMap.Builder<String, Tuple2<Broadcast<List<byte[]>>, WindowedValueCoder<SideInputT>>>
+        broadcastVariablesBuilder = ImmutableMap.builder();
+    for (SideInputId sideInputId : stagePayload.getSideInputsList()) {
+      RunnerApi.Components components = stagePayload.getComponents();
+      String collectionId =
+          components
+              .getTransformsOrThrow(sideInputId.getTransformId())
+              .getInputsOrThrow(sideInputId.getLocalName());
+      Tuple2<Broadcast<List<byte[]>>, WindowedValueCoder<SideInputT>> tuple2 =
+          broadcastSideInput(collectionId, components, context);
+      broadcastVariablesBuilder.put(collectionId, tuple2);
+    }
+
+    SparkExecutableStageFunction<InputT, SideInputT> function =
+        new SparkExecutableStageFunction<>(
+            stagePayload, context.jobInfo, outputMap, broadcastVariablesBuilder.build());
     JavaRDD<RawUnionValue> staged = inputRdd.mapPartitions(function);
 
     for (String outputId : outputs.values()) {
@@ -189,6 +210,49 @@ public class SparkBatchPortablePipelineTranslator {
           staged.flatMap(new SparkExecutableStageExtractionFunction<>(outputMap.get(outputId)));
       context.pushDataset(outputId, new BoundedDataset<>(outputRdd));
     }
+  }
+
+  /**
+   * Collect and serialize the data and then broadcast the result. *This can be expensive.*
+   *
+   * @return Spark broadcast variable and coder to decode its contents
+   */
+  private static <T> Tuple2<Broadcast<List<byte[]>>, WindowedValueCoder<T>> broadcastSideInput(
+      String collectionId, RunnerApi.Components components, SparkTranslationContext context) {
+    PCollection collection = components.getPcollectionsOrThrow(collectionId);
+    @SuppressWarnings("unchecked")
+    BoundedDataset<T> dataset = (BoundedDataset<T>) context.popDataset(collectionId);
+    PCollectionNode collectionNode = PipelineNode.pCollection(collectionId, collection);
+    WindowedValueCoder<T> coder;
+    try {
+      coder =
+          (WindowedValueCoder<T>) WireCoders.instantiateRunnerWireCoder(collectionNode, components);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    List<byte[]> bytes = dataset.getBytes(coder);
+    Broadcast<List<byte[]>> broadcast = context.getSparkContext().broadcast(bytes);
+    return new Tuple2<>(broadcast, coder);
+  }
+
+  private static <T> void translateFlatten(
+      PTransformNode transformNode, RunnerApi.Pipeline pipeline, SparkTranslationContext context) {
+
+    Map<String, String> inputsMap = transformNode.getTransform().getInputsMap();
+
+    JavaRDD<WindowedValue<T>> unionRDD;
+    if (inputsMap.isEmpty()) {
+      unionRDD = context.getSparkContext().emptyRDD();
+    } else {
+      JavaRDD<WindowedValue<T>>[] rdds = new JavaRDD[inputsMap.size()];
+      int index = 0;
+      for (String inputId : inputsMap.values()) {
+        rdds[index] = ((BoundedDataset<T>) context.popDataset(inputId)).getRDD();
+        index++;
+      }
+      unionRDD = context.getSparkContext().union(rdds);
+    }
+    context.pushDataset(getOutputId(transformNode), new BoundedDataset<>(unionRDD));
   }
 
   @Nullable
