9c0dfed000af058fb18c533130c240c1eeb7f0d7
Alex Barreto <axbarreto@lavabit.com>
2019-06-15 17:10:30 -0700

    Merge pull request #8850: [BEAM-7144] Fix for rescaling problem on Flink >= 1.6

--- sdks/java/core/src/test/java/org/apache/beam/sdk/coders/FloatCoderTest.java
+++ sdks/java/core/src/test/java/org/apache/beam/sdk/coders/FloatCoderTest.java
@@ -17,22 +17,76 @@
  */
 package org.apache.beam.sdk.coders;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
 
+import java.util.Arrays;
+import java.util.List;
+import org.apache.beam.sdk.testing.CoderProperties;
+import org.apache.beam.sdk.util.CoderUtils;
+import org.apache.beam.sdk.values.TypeDescriptor;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-/** Tests for {@link FloatCoder}. */
+/** Test case for {@link FloatCoder}. */
 @RunWith(JUnit4.class)
 public class FloatCoderTest {
 
   private static final Coder<Float> TEST_CODER = FloatCoder.of();
 
+  private static final List<Float> TEST_VALUES =
+      Arrays.asList(
+          0.0f,
+          -0.5f,
+          0.5f,
+          0.3f,
+          -0.3f,
+          1.0f,
+          -43.895687f,
+          (float) Math.PI,
+          Float.MAX_VALUE,
+          Float.MIN_VALUE,
+          Float.POSITIVE_INFINITY,
+          Float.NEGATIVE_INFINITY,
+          Float.NaN);
+
   @Test
-  public void testStructuralValueReturnTheSameValue() {
-    Float expected = 23.45F;
-    Object actual = TEST_CODER.structuralValue(expected);
-    assertEquals(expected, actual);
+  public void testDecodeEncodeEqual() throws Exception {
+    for (Float value : TEST_VALUES) {
+      CoderProperties.coderDecodeEncodeEqual(TEST_CODER, value);
+    }
+  }
+
+  /**
+   * Generated data to check that the wire format has not changed. To regenerate, see {@link
+   * org.apache.beam.sdk.coders.PrintBase64Encodings}.
+   */
+  private static final List<String> TEST_ENCODINGS =
+      Arrays.asList(
+          "AAAAAA", "vwAAAA", "PwAAAA", "PpmZmg", "vpmZmg", "P4AAAA", "wi-VLw", "QEkP2w", "f3___w",
+          "AAAAAQ", "f4AAAA", "_4AAAA", "f8AAAA");
+
+  @Test
+  public void testWireFormatEncode() throws Exception {
+    System.out.println(TEST_ENCODINGS);
+    CoderProperties.coderEncodesBase64(TEST_CODER, TEST_VALUES, TEST_ENCODINGS);
+  }
+
+  @Rule public ExpectedException thrown = ExpectedException.none();
+
+  @Test
+  public void encodeNullThrowsCoderException() throws Exception {
+    thrown.expect(CoderException.class);
+    thrown.expectMessage("cannot encode a null Float");
+
+    CoderUtils.encodeToBase64(TEST_CODER, null);
+  }
+
+  @Test
+  public void testEncodedTypeDescriptor() throws Exception {
+    assertThat(TEST_CODER.getEncodedTypeDescriptor(), equalTo(TypeDescriptor.of(Float.class)));
   }
 }
