982df646287e277078f98e44f698d76c2b69b883
Alex Barreto <axbarreto@lavabit.com>
2019-11-08 14:01:07 -0800

    Merge pull request #9665: [BEAM-2879] Support writing data to BigQuery via avro

--- sdks/python/apache_beam/transforms/transforms_keyword_only_args_test_py3.py
+++ sdks/python/apache_beam/transforms/transforms_keyword_only_args_test_py3.py
@@ -49,37 +49,37 @@ class KeywordOnlyArgsTests(unittest.TestCase):
     result1 = pcol | 'compute1' >> beam.FlatMap(
         sort_with_side_inputs,
         beam.pvalue.AsList(side), reverse=True)
-    assert_that(result1, equal_to([[4,3,1], [4,3,2]]), label='assert1')
+    assert_that(result1, equal_to([[4, 3, 1], [4, 3, 2]]), label='assert1')
 
     result2 = pcol | 'compute2' >> beam.FlatMap(
-      sort_with_side_inputs,
-      beam.pvalue.AsList(side))
-    assert_that(result2, equal_to([[1,3,4], [2,3,4]]), label='assert2')
+        sort_with_side_inputs,
+        beam.pvalue.AsList(side))
+    assert_that(result2, equal_to([[1, 3, 4], [2, 3, 4]]), label='assert2')
 
     result3 = pcol | 'compute3' >> beam.FlatMap(
-      sort_with_side_inputs)
+        sort_with_side_inputs)
     assert_that(result3, equal_to([]), label='assert3')
 
     result4 = pcol | 'compute4' >> beam.FlatMap(
-      sort_with_side_inputs, reverse=True)
+        sort_with_side_inputs, reverse=True)
     assert_that(result4, equal_to([]), label='assert4')
 
     result5 = pcol | 'compute5' >> beam.FlatMap(
-      sort_with_side_inputs_without_default_values,
-      beam.pvalue.AsList(side), reverse=True)
+        sort_with_side_inputs_without_default_values,
+        beam.pvalue.AsList(side), reverse=True)
     assert_that(result5, equal_to([[4, 3, 1], [4, 3, 2]]), label='assert5')
 
     result6 = pcol | 'compute6' >> beam.FlatMap(
-      sort_with_side_inputs_without_default_values,
-      beam.pvalue.AsList(side), reverse=False)
+        sort_with_side_inputs_without_default_values,
+        beam.pvalue.AsList(side), reverse=False)
     assert_that(result6, equal_to([[1, 3, 4], [2, 3, 4]]), label='assert6')
 
     result7 = pcol | 'compute7' >> beam.FlatMap(
-      sort_with_side_inputs_without_default_values, reverse=False)
+        sort_with_side_inputs_without_default_values, reverse=False)
     assert_that(result7, equal_to([]), label='assert7')
 
     result8 = pcol | 'compute8' >> beam.FlatMap(
-      sort_with_side_inputs_without_default_values, reverse=True)
+        sort_with_side_inputs_without_default_values, reverse=True)
     assert_that(result8, equal_to([]), label='assert8')
 
     pipeline.run()
@@ -100,13 +100,13 @@ class KeywordOnlyArgsTests(unittest.TestCase):
     result3 = pcoll | 'sum3' >> beam.CombineGlobally(bounded_sum)
     result4 = pcoll | 'sum4' >> beam.CombineGlobally(bounded_sum, bound=5)
     result5 = pcoll | 'sum5' >> beam.CombineGlobally(
-      bounded_sum_without_default_values, 5, 8, bound=20)
+        bounded_sum_without_default_values, 5, 8, bound=20)
     result6 = pcoll | 'sum6' >> beam.CombineGlobally(
-      bounded_sum_without_default_values, 0, 0, bound=500)
+        bounded_sum_without_default_values, 0, 0, bound=500)
     result7 = pcoll | 'sum7' >> beam.CombineGlobally(
-      bounded_sum_without_default_values, bound=500)
+        bounded_sum_without_default_values, bound=500)
     result8 = pcoll | 'sum8' >> beam.CombineGlobally(
-      bounded_sum_without_default_values, bound=5)
+        bounded_sum_without_default_values, bound=5)
 
     assert_that(result1, equal_to([20]), label='assert1')
     assert_that(result2, equal_to([10]), label='assert2')
@@ -132,10 +132,10 @@ class KeywordOnlyArgsTests(unittest.TestCase):
     result3 = pcoll | 'sum3' >> beam.ParDo(MyDoFn())
     result4 = pcoll | 'sum4' >> beam.ParDo(MyDoFn(), bound=5)
 
-    assert_that(result1, equal_to([15,15,14]), label='assert1')
-    assert_that(result2, equal_to([19,16,14]), label='assert2')
-    assert_that(result3, equal_to([6,3,1]), label='assert3')
-    assert_that(result4, equal_to([5,3,1]), label='assert4')
+    assert_that(result1, equal_to([15, 15, 14]), label='assert1')
+    assert_that(result2, equal_to([19, 16, 14]), label='assert2')
+    assert_that(result3, equal_to([6, 3, 1]), label='assert3')
+    assert_that(result4, equal_to([5, 3, 1]), label='assert4')
     pipeline.run()
 
 
