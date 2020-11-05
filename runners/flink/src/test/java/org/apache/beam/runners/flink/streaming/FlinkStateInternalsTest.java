ac6aa04ddcfd9bdcab530be1dd2a2de9e0b5c2d9
Alex Barreto <axbarreto@lavabit.com>
2020-11-05 09:56:36 -0600

    Merge pull request #13269: [BEAM-11191] fix ClassCastException when clearing watermark state

--- runners/flink/src/test/java/org/apache/beam/runners/flink/streaming/FlinkStateInternalsTest.java
+++ runners/flink/src/test/java/org/apache/beam/runners/flink/streaming/FlinkStateInternalsTest.java
@@ -57,7 +57,10 @@ import org.junit.runners.JUnit4;
 
 /** Tests for {@link FlinkStateInternals}. This is based on {@link StateInternalsTest}. */
 @RunWith(JUnit4.class)
-@SuppressWarnings("nullness") // TODO(https://issues.apache.org/jira/browse/BEAM-10402)
+@SuppressWarnings({
+  "rawtypes", // TODO(https://issues.apache.org/jira/browse/BEAM-10556)
+  "nullness" // TODO(https://issues.apache.org/jira/browse/BEAM-10402)
+})
 public class FlinkStateInternalsTest extends StateInternalsTest {
 
   @Override
@@ -165,9 +168,25 @@ public class FlinkStateInternalsTest extends StateInternalsTest {
     assertThat(stateInternals.minWatermarkHoldMs(), is(noHold.getMillis()));
   }
 
+  @Test
+  public void testGlobalWindowWatermarkHoldClear() throws Exception {
+    KeyedStateBackend<ByteBuffer> keyedStateBackend = createStateBackend();
+    FlinkStateInternals<String> stateInternals =
+        new FlinkStateInternals<>(
+            keyedStateBackend,
+            StringUtf8Coder.of(),
+            new SerializablePipelineOptions(FlinkPipelineOptions.defaults()));
+    StateTag<WatermarkHoldState> stateTag =
+        StateTags.watermarkStateInternal("hold", TimestampCombiner.EARLIEST);
+    Instant now = Instant.now();
+    WatermarkHoldState state = stateInternals.state(StateNamespaces.global(), stateTag);
+    state.add(now);
+    stateInternals.clearGlobalState();
+    assertThat(state.read(), is((Instant) null));
+  }
+
   public static KeyedStateBackend<ByteBuffer> createStateBackend() throws Exception {
     MemoryStateBackend backend = new MemoryStateBackend();
-
     AbstractKeyedStateBackend<ByteBuffer> keyedStateBackend =
         backend.createKeyedStateBackend(
             new DummyEnvironment("test", 1, 0),
